ed5df409069000ccf55cd49502bfe5839679badb
nguyenquilong <35983478+nguyenquilong@users.noreply.github.com>
2021-09-12 20:31:06 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,38 +1,26 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { Counter } from "k6/metrics";
+import { Counter, Rate } from "k6/metrics";
 
 let ErrorCount = new Counter("errors");
+let failures = new Rate('failed request')
 
 export const options = {
   vus: 2,
-  duration: "5s",
+  duration: "30s",
   thresholds: {
-    errors: ["count>10"]
-  }
+    failed_requests : ['rate<=0'],
+    http_req_duration: ['p(95)<1000'],
+    errors: ["count<10"],
+  },
 };
 
-// export default function() {
-//   const path = Math.random() < 0.9 ? "200" : "500";
-
-//   let res = http.get(`https://httpbin.test.loadimpact.com/status/${path}`);
-//   let success = check(res, {
-//     "status is 200": r => r.status === 200
-//   });
-//   if (!success) {
-//     ErrorCount.add(1);
-//   }
-
-//   sleep(2);
-// }
-
 export default function () {
-  var res = http.post('https://webapi.dantri.com.vn/bn/list-pos', {"list":[{"category":0,"position":24}]});
-  console.log('Response time was ' + String(res.timings.duration) + ' ms');
-  check(res, {
-    'is status 200': r => r.status === 200,
-  })
-  if(res.timings.duration > 100) {
-    ErrorCount.add(1);
-  }
+  const result = http.get("https://test-api.k6.io/");
+  check(result, {
+    "http response status code 200": (r) => r.status === 200,
+  });
+  failures.add(result.status !== 200)
+  ErrorCount.add(result.status !== 200)
+  sleep(1)
 }
